c8b4405f9ae7d1eca0d2a24df1cc318886a7ec98
Abhishek Kr Yadav <manofsteel@Abhisheks-MacBook-Pro.local>
2020-03-11 02:36:25 +0530

    adding schema and schema validator

--- app/api/shortner/schemas/create_short_url.py
+++ app/api/shortner/schemas/create_short_url.py
@@ -1,3 +1,9 @@
 CreateShortUrlSchema = {
-
+    'type': 'object',
+    'properties': {
+        'longUrl': {'type': 'string'},
+        'customName': {'type': 'string'},
+        'expiryInMin': {'type': 'integer'},
+    },
+    'required': ['longUrl']
 }
